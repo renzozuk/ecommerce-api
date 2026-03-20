2bf14a2e664ae5dbcac95279db0446bd5857b75e
Renzo Zukeram <renzo.zuk19@gmail.com>
2026-03-19 21:43:45 -0300

    feat: Add Customer entity

---
+++ src/main/kotlin/com/renzozukeram/ecommerce/model/entities/Customer.kt
@@ -0,0 +1,32 @@
+package com.renzozukeram.ecommerce.model.entities
+
+import jakarta.persistence.Column
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import java.time.LocalDateTime
+
+
+@Table(name = "customers")
+data class Customer (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: String? = null,
+
+    @Column(nullable = false)
+    var name: String,
+
+    @Column(nullable = false, unique = true)
+    var email: String,
+
+    @Column(name = "phone_number")
+    var phoneNumber: String,
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @Column(name = "updated_at")
+    var updatedAt: LocalDateTime? = null
+
+)
