132fa1b12ee8df012fcc1e521272525a68db30b5
Renzo Zukeram <renzo.zuk19@gmail.com>
2026-03-22 18:14:40 -0300

    feat: Add functions in repositories

--- src/main/kotlin/com/renzozukeram/ecommerce/repositories/CustomerRepository.kt
+++ src/main/kotlin/com/renzozukeram/ecommerce/repositories/CustomerRepository.kt
@@ -2,6 +2,9 @@ package com.renzozukeram.ecommerce.repositories
 
 import com.renzozukeram.ecommerce.model.entities.Customer
 import org.springframework.data.repository.CrudRepository
-import java.util.UUID
+import java.util.*
 
-interface CustomerRepository : CrudRepository<Customer, UUID>
+interface CustomerRepository : CrudRepository<Customer, UUID> {
+    fun existsByEmail(email: String): Boolean
+    fun findByEmail(email: String): Customer?
+}
--- src/main/kotlin/com/renzozukeram/ecommerce/repositories/OrderRepository.kt
+++ src/main/kotlin/com/renzozukeram/ecommerce/repositories/OrderRepository.kt
@@ -1,7 +1,25 @@
 package com.renzozukeram.ecommerce.repositories
 
 import com.renzozukeram.ecommerce.model.entities.Order
+import com.renzozukeram.ecommerce.model.entities.OrderStatus
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.CrudRepository
+import java.time.LocalDateTime
 import java.util.UUID
 
-interface OrderRepository : CrudRepository<Order, UUID>
+interface OrderRepository : CrudRepository<Order, UUID> {
+    fun findByCustomerId(customerId: UUID, pageable: Pageable): Page<Order>
+
+    fun findByOrderDateBetween(startDate: LocalDateTime, endDate: LocalDateTime, pageable: Pageable): Page<Order>
+
+    fun findByOrderDateBetweenAndStatus(
+        startDate: LocalDateTime,
+        endDate: LocalDateTime,
+        status: OrderStatus
+    ): List<Order>
+
+    fun countByStatus(status: OrderStatus): Long
+
+    fun existsByCustomerId(customerId: UUID): Boolean
+}
