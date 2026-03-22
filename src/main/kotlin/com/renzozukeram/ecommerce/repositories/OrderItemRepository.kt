45741780d676b58c346312ef25384fd1ef23302f
Renzo Zukeram <renzo.zuk19@gmail.com>
2026-03-22 16:32:46 -0300

    chore: Delete OrderItemRepository

--- src/main/kotlin/com/renzozukeram/ecommerce/repositories/OrderItemRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.renzozukeram.ecommerce.repositories
-
-import com.renzozukeram.ecommerce.model.entities.OrderItem
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-import java.util.UUID
-
-@Repository
-interface OrderItemRepository : CrudRepository<OrderItem, UUID>
